7bc898c51166bae3590dc9f2d91f67f1172bc8bf
Dharmanand <Dharmanand@users.noreply.github.com>
2018-06-15 15:18:43 +0530

    Add files via upload
    Added all files !!

---
+++ src/main/java/com/example/demo/handler/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.demo.handler.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.exception.BaseException;
+
+/**
+ * @author ekansh
+ * @since 19/2/16
+ */
+@ControllerAdvice
+@RestController
+public class GlobalExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = BaseException.class)
+    public String handleBaseException(BaseException e){
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public String handleException(Exception e){return e.getMessage();}
+
+
+}
